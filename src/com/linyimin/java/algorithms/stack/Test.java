9dbf786a4c906e8767160944ec7637bafbaef5c0
lym152898 <lin_yi_min@163.com>
2018-06-21 22:08:40 +0800

    添加泛型测试

--- src/com/linyimin/java/algorithms/stack/Test.java
+++ src/com/linyimin/java/algorithms/stack/Test.java
@@ -4,16 +4,29 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String [] args){
-        FixedCapacityStackOfString stack = new FixedCapacityStackOfString(10);
+//        FixedCapacityStackOfString stack = new FixedCapacityStackOfString(10);
+//        Scanner sin = new Scanner(System.in);
+//        while(sin.hasNext()){
+//            String s = sin.next();
+//            if(! s.equals("-")){
+//                stack.push(s);
+//            }else{
+//                break;
+//            }
+//
+//        }
+//        System.out.println("size=" + stack.size());
+//        int len = stack.size();
+//        for(int i = 0; i < len; i++){
+//            System.out.println(stack.pop());
+//        }
+//        System.out.println("size=" + stack.size());
+//    }
+        FixedCapacityStack<Double> stack = new FixedCapacityStack(10);
         Scanner sin = new Scanner(System.in);
         while(sin.hasNext()){
-            String s = sin.next();
-            if(! s.equals("-")){
-                stack.push(s);
-            }else{
-                break;
-            }
-
+            double s = sin.nextDouble();
+            stack.push(s);
         }
         System.out.println("size=" + stack.size());
         int len = stack.size();
