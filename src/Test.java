2aa0d2bd6aa408a8f3d6978f9156eac3f48d33f0
lym152898 <linyimin520812@gmail.com>
2018-07-01 23:01:22 +0800

    pack和unpack的测试

---
+++ src/Test.java
@@ -0,0 +1,40 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @Author: linyimin
+ * @Description:
+ * @Date: Created in 下午4:42 18-7-1
+ */
+public class Test {
+    public static Thread thread;
+    public static void main(String [] args) throws InterruptedException {
+       Thread park = new Thread(new Park(), "park");
+       Thread unpark = new Thread(new Unpark(), "unpark");
+       park.start();
+       unpark.start();
+    }
+
+    static class Park implements Runnable{
+
+        @Override
+        public void run() {
+            thread = Thread.currentThread();
+            LockSupport.park();
+            System.out.println("阻塞结束");
+        }
+    }
+
+    static class Unpark implements Runnable{
+        @Override
+        public void run() {
+            System.out.println("Park阻塞5秒");
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            LockSupport.unpark(thread);
+        }
+    }
+}
